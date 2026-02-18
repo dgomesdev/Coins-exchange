ef4b344814fc22b57e92aa6c1e1bcc8b867ffc52
dgomesdev <danilo.gomes@outlook.fr>
2026-02-18 13:15:03 -0400

    Improved repository

--- app/src/main/java/com/dgomesdev/exchangeapp/data/repository/ExchangeRepositoryImpl.kt
+++ app/src/main/java/com/dgomesdev/exchangeapp/data/repository/ExchangeRepositoryImpl.kt
@@ -1,45 +1,81 @@
 package com.dgomesdev.exchangeapp.data.repository
 
+import android.util.Log
 import com.dgomesdev.exchangeapp.data.local.ExchangeLocalDataSource
 import com.dgomesdev.exchangeapp.data.remote.ExchangeRemoteDataSource
 import com.dgomesdev.exchangeapp.domain.ConversionPair
 import com.dgomesdev.exchangeapp.domain.ExchangeModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.flow
+import com.dgomesdev.exchangeapp.domain.RepositoryError
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.Locale
 import javax.inject.Inject
 
 class ExchangeRepositoryImpl @Inject constructor(
     private val remoteDataSource: ExchangeRemoteDataSource,
     private val localDataSource: ExchangeLocalDataSource
 ) : ExchangeRepository {
-    override fun getValues(conversionPairs: List<ConversionPair>): Flow<List<ExchangeModel>> {
-        if (conversionPairs.isEmpty()) {
-            throw IllegalArgumentException("Coins list can't be empty")
-        }
+    override suspend fun getValues(conversionPairs: List<ConversionPair>): Result<List<ExchangeModel>> {
+        return runCatching {
+            if (conversionPairs.isEmpty()) {
+                throw IllegalArgumentException("Coins list can't be empty")
+            }
 
-        val coinsList: String = conversionPairs.joinToString(separator = ",") { it.coins }
+            try {
+                if (!shouldFetchFromApi()) {
+                    Log.d("ExchangeRepositoryImpl", "Fetching from local database")
+                    return@runCatching fetchFromLocalDatabase()
+                }
 
-        val remoteFetchAndSaveFlow = flow {
-            val remoteResult = remoteDataSource.getExchangeValues(coinsList).map { entity ->
-                ExchangeModel.fromRemoteEntity(entity)
+                val coinsList: String = conversionPairs.joinToString(separator = ",") { it.coins }
+                val remoteResponse = remoteDataSource.getExchangeValues(coinsList).getOrElse {
+                    return@runCatching try {
+                        fetchFromLocalDatabase()
+                    } catch (e: Exception) {
+                        throw RepositoryError("${e.message}", e)
+                    }
+                }
+                Log.d("ExchangeRepositoryImpl", "Fetched from API")
+                val values = remoteResponse.map {
+                    ExchangeModel.fromRemoteEntity(it)
+                }
+                val localEntities = values.map { model ->
+                    model.toLocalEntity()
+                }
+                localEntities.forEach {
+                    Log.d("ExchangeRepositoryImpl", "Local entity: $it")
+                }
+                localDataSource.saveExchangeData(localEntities)
+                Log.d("ExchangeRepositoryImpl", "Saved to local database")
+                values
+            } catch (e: Exception) {
+                throw RepositoryError("${e.message}", e)
             }
-            remoteResult.forEach { exchangeModel ->
-                localDataSource.save(exchangeModel.toLocalEntity())
-            }
-            emit(remoteResult)
-        }
-
-        return remoteFetchAndSaveFlow.catch { error ->
-            val currentLocalEntities = localDataSource.getAll().firstOrNull()
-            if (currentLocalEntities.isNullOrEmpty()) {
-                throw RuntimeException("Failed to fetch from remote, and no local data found.")
-            }
-            val localResult = currentLocalEntities.map { entity ->
-                ExchangeModel.fromLocalEntity(entity)
-            }
-            emit(localResult)
         }
     }
+
+    override suspend fun getFormattedLastUpdatedDate(): String {
+        val lastUpdatedTimestamp = localDataSource.getLastUpdatedTimestamp()?.timestamp ?: return ""
+        val lastUpdatedZonedDateTime = ZonedDateTime
+            .ofInstant(Instant.ofEpochMilli(lastUpdatedTimestamp), ZoneId.systemDefault())
+        val formattedDate = DateTimeFormatter
+            .ofLocalizedDateTime(FormatStyle.SHORT)
+            .withLocale(Locale.getDefault())
+        return lastUpdatedZonedDateTime.format(formattedDate)
+    }
+
+    private suspend fun shouldFetchFromApi(): Boolean {
+        val lastUpdatedTimestamp = localDataSource.getLastUpdatedTimestamp() ?: return true
+        val currentTime = System.currentTimeMillis()
+        val timeDifference = currentTime - lastUpdatedTimestamp.timestamp
+        return timeDifference >= 60 * 60 * 1000
+    }
+
+    private suspend fun fetchFromLocalDatabase() =
+        localDataSource.getAllExchangeValues().map { localEntity ->
+            ExchangeModel.fromLocalEntity(localEntity)
+        }
 }
