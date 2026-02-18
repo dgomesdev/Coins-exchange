24b22bb73ca5d4cae6c203b4cfefde8769300c50
dgomesdev <danilo.gomes@outlook.fr>
2026-02-18 13:14:42 -0400

    Improved remote Data source

--- app/src/main/java/com/dgomesdev/exchangeapp/data/remote/ExchangeRemoteDataSource.kt
+++ app/src/main/java/com/dgomesdev/exchangeapp/data/remote/ExchangeRemoteDataSource.kt
@@ -1,5 +1,5 @@
 package com.dgomesdev.exchangeapp.data.remote
 
 interface ExchangeRemoteDataSource {
-    suspend fun getExchangeValues(coins: String): ExchangeResponse
+    suspend fun getExchangeValues(coins: String): Result<ExchangeResponse>
 }
--- app/src/main/java/com/dgomesdev/exchangeapp/data/remote/ExchangeRemoteDataSourceImpl.kt
+++ app/src/main/java/com/dgomesdev/exchangeapp/data/remote/ExchangeRemoteDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.dgomesdev.exchangeapp.data.remote
 
+import android.util.Log
 import com.dgomesdev.exchangeapp.domain.ClientErrorException
 import com.dgomesdev.exchangeapp.domain.CoinNotFoundException
 import com.dgomesdev.exchangeapp.domain.InvalidCoinsException
@@ -15,54 +16,58 @@ import javax.inject.Inject
 class ExchangeRemoteDataSourceImpl @Inject constructor(
     private val api: ExchangeApi
 ) : ExchangeRemoteDataSource {
-    override suspend fun getExchangeValues(coins: String): ExchangeResponse {
-        try {
-            if (coins.isBlank()) {
-                throw RuntimeException("Invalid coins.")
-            }
-            return this.api.getExchangeValues(coins)
-        } catch (e: HttpException) {
-            val httpCode = e.code()
-            when (httpCode) {
-                404 -> {
-                    throw CoinNotFoundException(
-                        message = "Coins Not Found Error: $httpCode - Coins not found",
-                        cause = e
-                    )
+    override suspend fun getExchangeValues(coins: String): Result<ExchangeResponse> {
+        Log.i("ExchangeRemoteDataSourceImpl", "getExchangeValues: $coins")
+        return runCatching {
+            try {
+                if (coins.isBlank()) {
+                    throw RuntimeException("Invalid coins.")
                 }
-                in 400..499 -> {
-                    throw ClientErrorException(
-                        httpStatusCode = httpCode,
-                        cause = e
-                    )
+                val result = api.getExchangeValues(coins)
+                Log.i("ExchangeRemoteDataSourceImpl", "result: $result")
+                result
+            } catch (e: HttpException) {
+                when (val httpCode = e.code()) {
+                    404 -> {
+                        throw CoinNotFoundException(
+                            message = "Coins Not Found Error: $httpCode - Coins not found",
+                            cause = e
+                        )
+                    }
+                    in 400..499 -> {
+                        throw ClientErrorException(
+                            httpStatusCode = httpCode,
+                            cause = e
+                        )
+                    }
+                    in 500..599 -> {
+                        throw ServerErrorException(
+                            httpStatusCode = httpCode,
+                            cause = e
+                        )
+                    }
+                    else -> {
+                        throw UnexpectedApiException("Unexpected HTTP error: $httpCode", e)
+                    }
                 }
-                in 500..599 -> { // Server errors
-                    throw ServerErrorException(
-                        httpStatusCode = httpCode,
-                        cause = e
-                    )
+            } catch (e: SocketTimeoutException) {
+                throw TimeoutException(cause = e)
+            } catch (e: IOException) {
+                throw NetworkErrorException(cause = e)
+            } catch (e: RuntimeException) {
+                if (e.message == "Invalid coins.") {
+                    throw InvalidCoinsException(message = e.message ?: "Invalid coins.", cause = e)
                 }
-                else -> {
-                    throw UnexpectedApiException("Unexpected HTTP error: $httpCode", e)
+                if (e.cause is SocketTimeoutException) {
+                    throw TimeoutException(message = "${e.cause?.message}", cause = e.cause)
                 }
+                if (e.cause is IOException) {
+                    throw NetworkErrorException(message = "${e.cause?.message}", cause = e.cause)
+                }
+                throw UnexpectedApiException("An unexpected runtime error occurred", e)
+            } catch (e: Exception) {
+                throw UnexpectedApiException("An unexpected error occurred", e)
             }
-        } catch (e: SocketTimeoutException) {
-            throw TimeoutException(cause = e)
-        } catch (e: IOException) {
-            throw NetworkErrorException(cause = e)
-        } catch (e: RuntimeException) {
-            if (e.message == "Invalid coins.") {
-                throw InvalidCoinsException(message = e.message ?: "Invalid coins.", cause = e)
-            }
-            if (e.cause is SocketTimeoutException) {
-                throw TimeoutException(message = "${e.cause?.message}", cause = e.cause)
-            }
-            if (e.cause is IOException) {
-                throw NetworkErrorException(message = "${e.cause?.message}", cause = e.cause)
-            }
-            throw UnexpectedApiException("An unexpected runtime error occurred", e)
-        } catch (e: Exception) {
-            throw UnexpectedApiException("An unexpected error occurred", e)
         }
     }
 }
