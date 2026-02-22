eec94439ee3b9fb9d3af770311371c9090b44335
dgomesdev <danilo.gomes@outlook.fr>
2026-02-22 14:32:25 -0400

    Upgraded version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dgomesdev.exchangeapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
